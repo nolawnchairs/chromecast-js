c37fdef20c5aa3e523a50683a4a7783d70a7859d
mwieczorek <nolawnchairs@gmail.com>
2019-01-16 18:23:57 +0700

    removed registrar

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 "use strict";
 
 var cc = require('./lib/Chromecast')
-var re = require('./lib/Registrar')
 var co = require('./lib/Controller')
 
 module.exports =  {
   Chromecast: cc.default,
   CastOptions: cc.CastOptions,
-  Register: re.default,
   Controller: co.default
 }
 
